59e842432a19b91f5d1d83596d00e290b7dc359a
Phoebe Oigara <phoebeoigara@gmail.com>
2023-04-21 12:28:59 +0300

    Created the bonus feature that adds emoji based on the number of minutes a read takes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,17 @@
 function Article ({title, date ="January 1, 1970" ,minutes, preview}) {
+   
+    let readTime = "";
+  if (minutes < 30) {
+    const coffeeCups = Math.ceil(minutes / 5);
+    readTime = "☕️".repeat(coffeeCups);
+  } else {
+    const bentoBoxes = Math.ceil(minutes / 10);
+    readTime = "🍱".repeat(bentoBoxes);
+  }
+    
     return( <article key="id">
          <h3>{title}</h3>
-         <small>{date}: {minutes} minutes read</small>
+         <small>{date}: {minutes}, {readTime}minutes read</small>
          <p>{preview}</p>
 
      </article>)
