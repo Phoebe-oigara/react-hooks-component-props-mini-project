62deebaa3aa72524d5e3f705aaca8324d6ddcd6a
Phoebe Oigara <phoebeoigara@gmail.com>
2023-04-21 11:19:08 +0300

    Creating components, passing props to child elements, destructuring

---
+++ src/components/ArticleList.js
@@ -0,0 +1,26 @@
+import Article from "./Article"
+
+function ArticleList({posts}) {
+      
+    return (
+       <main>
+        {posts.map((post) => {
+            return (
+                <Article
+                title={post.title}
+                date={post.date}
+                preview={post.preview}
+                />
+            )
+        })}
+
+       
+       </main>
+
+      
+    )
+}
+
+
+
+export default ArticleList
